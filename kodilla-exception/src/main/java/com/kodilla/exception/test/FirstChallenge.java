e87bee9a9ce72f7241425cf86a633a7ae0b2a523
panwoj <pan_woj@poczta.onet.pl>
2017-11-24 15:02:43 +0100

    TryCatch1

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,35 @@
+package com.kodilla.exception.test;
+
+import java.io.IOException;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        try {
+            if(b == 0){
+                throw new ArithmeticException();
+            }
+            return a / b;
+        } catch (ArithmeticException e) {
+            System.out.println("!!!Error: " + e + "!!!");
+            return 0;
+        } finally {
+            System.out.println("Thanks for using our program.");
+
+        }
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        double result = firstChallenge.divide(3, 0);
+
+        System.out.println(result);
+
+    }
+}
