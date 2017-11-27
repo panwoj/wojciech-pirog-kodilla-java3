9bdb6e1e103d9e3fba17c481d69960d9ac45e24f
panwoj <pan_woj@poczta.onet.pl>
2017-11-27 21:35:30 +0100

    TryCatch2

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -5,31 +5,36 @@ import java.io.IOException;
 public class FirstChallenge {
 
     public double divide(double a, double b) throws ArithmeticException {
-        try {
-            if(b == 0){
-                throw new ArithmeticException("aaa");
-            }
-            return a / b;
-        } catch (ArithmeticException e) {
-            System.out.println("!!!Error: " + e + "!!!");
-            return 0;
-        } finally {
-            System.out.println("Thanks for using our program.");
-
+        if (b == 0) {
+            throw new ArithmeticException("aaa");
         }
+        return a / b;
     }
 
     /**
      * This main can throw an ArithmeticException!!!
+     *
      * @param args
      */
     public static void main(String[] args) {
 
         FirstChallenge firstChallenge = new FirstChallenge();
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("!!!Error: " + e + "!!!");
+        } finally {
+            System.out.println("Thanks for using our program.");
+        }
 
-        double result = firstChallenge.divide(3, 0);
-
-        System.out.println(result);
-
+        try {
+            double result2 = firstChallenge.divide(6, 2);
+            System.out.println(result2);
+        } catch (ArithmeticException e) {
+            System.out.println("!!!Error: " + e + "!!!");
+        } finally {
+            System.out.println("Thanks for using our program.");
+        }
     }
 }
