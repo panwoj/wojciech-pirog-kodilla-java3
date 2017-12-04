e3173542c9362edd15460361a7f3a1c23d01d203
panwoj <pan_woj@poczta.onet.pl>
2017-12-04 21:20:13 +0100

    newCommit

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flights/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flights/Application.java
@@ -15,5 +15,6 @@ public class Application {
 
         SearchProcessor searchProcessor3 = new SearchProcessor(new SearchConnectingFlights());
         searchProcessor3.process(search);
+        searchProcessor3.process(search);
     }
 }
