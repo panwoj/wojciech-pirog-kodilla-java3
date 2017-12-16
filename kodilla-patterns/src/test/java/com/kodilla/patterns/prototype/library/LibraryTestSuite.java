5afdb54251698698c3be7307d0782bd9c2b1a3f1
panwoj <pan_woj@poczta.onet.pl>
2017-12-16 06:01:26 +0100

    Prototype

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -0,0 +1,40 @@
+package com.kodilla.patterns.prototype.library;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
+
+public class LibraryTestSuite {
+    @Test
+    public void testGetBooks() {
+        //Given
+        //creating Books for Library
+        Library library = new Library("Library 1");
+        IntStream.iterate(1, n -> n+1)
+                .limit(20)
+                .forEach(b -> library.getBooks().add(
+                        new Book("Book number " + b, "Author " + (21 - b),
+                                LocalDate.now().minusYears(b).minusMonths(b*2).minusDays(3 * b))) );
+
+        //making a deep copy of object library
+        Library library2 = null;
+        try {
+            library2 = library.deepCopy();
+            library2.setName("Library 2");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        //When
+        library2.getBooks().add(
+                new Book("Book number 21", "Author 1", LocalDate.of(2010,10,11)));
+        //Then
+        System.out.println(library);
+        System.out.println(library2);
+        Assert.assertEquals(20, library.getBooks().size());
+        Assert.assertEquals(21, library2.getBooks().size());
+    }
+}
